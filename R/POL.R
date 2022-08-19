d24433df3330e4ee049b2048f764612374d95a06
Alexandre Michel <Alexandre.Michel@USherbrooke.ca>
2022-08-19 16:39:24 -0400

    installr removed because of conflict with devtools on "build"

--- R/POL.R
+++ R/POL.R
@@ -1,7 +1,6 @@
 #### CONFIGURATION ####
 
 requireNamespace("devtools", quietly = TRUE)
-requireNamespace("installr", quietly = TRUE)
 requireNamespace("conflicted", quietly = TRUE)
 requireNamespace("showtext", quietly = TRUE)
 requireNamespace("WDI", quietly = TRUE)
