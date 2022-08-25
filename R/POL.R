b78ad0c5bf2d1f8317c32dc76cf83b873913a398
Alexandre Michel <Alexandre.Michel@USherbrooke.ca>
2022-08-25 16:26:34 -0400

    forgot haven:: before read_dta

--- R/POL.R
+++ R/POL.R
@@ -359,7 +359,7 @@ importer = function(data = NULL, indicateurs = NULL, colonnes = TRUE, encodage =
       fichier = as.character(data)
     }
     cat("\n")
-    base = read_dta(fichier)
+    base = haven::read_dta(fichier)
   } else if (format == 6) {
     nbr_pays = as.numeric(readline(prompt = "Entrez le nombre de pays pour lesquels les données seront importées :  \n"))
     cat("\n")
